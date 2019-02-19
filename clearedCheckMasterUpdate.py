602e1b3f0dbf7eaded98fa0b6197e895e1a726e3
cepps <cepps@regionalmanagement.com>
2019-02-19 09:42:27 -0500

    Update clearedCheckMasterUpdate.py
    Removed unneeded package imports

--- clearedCheckMasterUpdate.py
+++ clearedCheckMasterUpdate.py
@@ -1,43 +1,9 @@
-import numpy as np
 import pandas as pd
-import dask.dataframe as dd
-import scipy.stats as stats
-import matplotlib.pyplot as plt
-import sklearn
-import statsmodels
-import statsmodels.api as sm
-import statsmodels.formula.api as smf   #FOR USING 'R'-STYLE FORMULAS FOR REGRESSIONS
-import imaplib, email, os
-import configparser
-import datetime
-from scipy.stats import norm
-from sklearn.preprocessing import StandardScaler
-from scipy import stats
-from pandas import DataFrame
-from pandas import Series
+import os
 import warnings
 warnings.filterwarnings('ignore')
-import seaborn as sns
-sns.set_style("whitegrid")
-sns.set_context("poster")
-color = sns.color_palette()
-
-# special matplotlib argument for improved plots
-from matplotlib import rcParams
-
-pd.set_option("display.max_rows",None)
-pd.set_option("display.max_columns", None)
-pd.set_option('display.float_format', lambda x: '%.2f' % x)
-
-import imaplib, email, os
-import configparser
-import datetime
-import shutil
-import os
 import glob
-
 import openpyxl
-import pprint
 
 
 path =r'\\prod-app02\ConvenienceCheckApproval\WellsFargoFiles' # use your path
